c732a3db2ddfa56b20788871d248ed4770eeb57d
Shazi-dani <shazinamunir26@gmail.com>
2024-07-10 02:04:29 +0500

    Migrate from using sqlitedb to MySQL DB

--- garage_management_system/garage_management_system/settings.py
+++ garage_management_system/garage_management_system/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'garage_management_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'garage_system',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
