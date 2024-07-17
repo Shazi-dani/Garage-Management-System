8e56e68e57b78405faffd9d868d9e746426cc00f
Shazi-dani <shazinamunir26@gmail.com>
2024-07-17 05:49:25 +0500

    Add User Admin view in Admin Panel

--- garage_management_system/users/admin.py
+++ garage_management_system/users/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import BlacklistedToken
+from .models import BlacklistedToken, CustomUser
 
 
 @admin.register(BlacklistedToken)
 class BlacklistedTokenAdmin(admin.ModelAdmin):
     list_display = ['token', 'blacklisted_at']
     readonly_fields = ['blacklisted_at']
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'contact_no', 'user_type']
