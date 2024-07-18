92c66f4f2557cca7539bfae422de343ad78ed20a
Shazi-dani <shazinamunir26@gmail.com>
2024-07-18 17:44:15 +0500

    Created Service, Vehicle and Appointment Serializers

--- garage_management_system/dashboard/serializers.py
+++ garage_management_system/dashboard/serializers.py
@@ -1,7 +1,24 @@
 from rest_framework import serializers
-from .models import Appointment
+from .models import Appointment, Service, Vehicle
+from users.serializers import CustomUserSerializer
+
+# Serializer for Vehicle model
+class VehicleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vehicle
+        fields = ['id', 'model', 'make', 'color', 'license_plate_no', 'vehicle_number', 'vehicle_type', 'description']  # Customize these fields
+
+# Serializer for Service model
+class ServiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Service
+        fields = ['description', 'cost']  # Define service-related fields
 
 class AppointmentSerializer(serializers.ModelSerializer):
+    user = CustomUserSerializer(read_only=True) # Nested serializer for Custom User
+    vehicle = VehicleSerializer(read_only=True) # Nested serializer for Vehicle
+    service = ServiceSerializer(read_only=True) # Nested serializer for Service
+
     class Meta:
         model = Appointment
-        fields = ['user', 'vehicle', 'service', 'appointment_date', 'status']
+        fields = ['id', 'user', 'vehicle', 'service', 'appointment_date', 'status']
