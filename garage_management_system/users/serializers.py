2027c28e80ceea8061ae3f495136348f68898397
Shazi-dani <shazinamunir26@gmail.com>
2024-07-18 17:43:28 +0500

    Added CustomUserSerializer

--- garage_management_system/users/serializers.py
+++ garage_management_system/users/serializers.py
@@ -2,7 +2,12 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 User = get_user_model()
-print(type(User))
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email','first_name', 'last_name', 'contact_no')
+
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
