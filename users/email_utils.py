adcfe366194a691ed925ca448676a429007fce67
shazina danish <shazinamunir26@gmail.com>
2024-07-22 13:30:47 +0000

    Update email template path

--- users/email_utils.py
+++ users/email_utils.py
@@ -5,7 +5,7 @@ from django.utils.html import strip_tags
 
 def send_signup_email(user):
     subject = 'Successful Registration on Shazina Garage Management System'
-    html_message = render_to_string('emails/register_email.html', {'user': user})
+    html_message = render_to_string('register_email.html', {'user': user})
     plain_message = strip_tags(html_message)
     from_email = os.environ.get('DEFAULT_FROM_EMAIL')
     to = user.email
